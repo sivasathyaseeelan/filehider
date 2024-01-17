89089fff9d814371e78199444343f10a1719885d
sivasathyaseeelan <dnsiva.sathyaseelan.chy21@iitbhu.ac.in>
2024-01-18 03:31:15 +0530

    initial commit

---
+++ src/main/java/db/MyConnection.java
@@ -0,0 +1,36 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class MyConnection {
+    public static Connection connection = null;
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/filehider?useSSL=false", "root", "sivaaa10");
+        }catch (ClassNotFoundException| SQLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Connected to db ...");
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if(connection != null){
+            try{
+                connection.close();
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MyConnection.getConnection();
+    }
+
+}
